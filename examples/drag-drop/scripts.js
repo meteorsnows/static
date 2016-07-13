86b21867ca7d65e04dcdd82557fdd0bcd1b78e26
Meteor <meteorsnows@gmail.com>
2016-07-13 17:57:46 +0800

    gh-pages: Add Jquery Organization Chart

---
+++ examples/drag-drop/scripts.js
@@ -0,0 +1,60 @@
+'use strict';
+
+(function($){
+
+  $(function() {
+
+var datascource = {
+  'name': 'Lao Lao',
+  'title': 'general manager',
+  'children': [
+    
+  ]
+};
+
+var countTree = 10;
+var topTree = 10
+var nextCount = 0;
+for(var i=1; i <= topTree;i++){
+
+  
+
+  var secondGen = { 'name': 'Bo Miao'+i, 'title': 'department manager',
+      'children': []
+    };
+  var thirdGen = [];
+
+  nextCount = 1 + (i * countTree);
+  
+  for(var j = nextCount ; j < (nextCount+countTree); j++){
+    var obj = {'name': null, 'title': null};
+    obj.name = 'people_' +j;
+    obj.title = 'people_' + j;
+    thirdGen.push(obj)
+  }
+  secondGen.children = thirdGen
+  datascource.children.push(secondGen);
+
+}
+
+    $('#chart-container').orgchart({
+      'data' : datascource,
+      'nodeContent': 'title',
+      'draggable': true,
+      'dropCriteria': function($draggedNode, $dragZone, $dropZone) {
+        if($draggedNode.find('.content').text().indexOf('manager') > -1 && $dropZone.find('.content').text().indexOf('engineer') > -1) {
+          return false;
+        }
+        return true;
+      }
+    })
+    .children('.orgchart').on('nodedropped.orgchart', function(event) {
+      console.log('draggedNode:' + event.draggedNode.children('.title').text()
+        + ', dragZone:' + event.dragZone.children('.title').text()
+        + ', dropZone:' + event.dropZone.children('.title').text()
+      );
+    });
+
+  });
+
+})(jQuery);
